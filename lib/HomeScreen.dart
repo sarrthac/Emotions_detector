0e9f3a623f61fa49edfade14e0f6b4b489b5bd88
Sarrthac <sarthakzalte@gmail.com>
2021-08-20 00:13:48 +0530

    App in working state

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+import 'main.dart';
 
 
 class Home extends StatefulWidget {
@@ -13,15 +16,83 @@ class _HomeState extends State<Home> {
   CameraImage? cameraImage;
   CameraController? cameraController;
   String output = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCamera();
+    loadmodel();
+  }
   
   loadCamera(){
-    cameraController = CameraController(description, resolutionPreset)
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if(!mounted){
+        return;
+      }else{
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+           cameraImage = imageStream;
+           runModel();
+          });
+        });
+      }
+    });  
+  }
+  
+  runModel() async{
+    if(cameraImage != null){
+      var predictions = await Tflite.runModelOnFrame(
+       bytesList: cameraImage!.planes.map((plane){
+         return plane.bytes;
+       }).toList(),
+      imageHeight: cameraImage!.height,
+      imageWidth: cameraImage!.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      rotation: 90,
+      numResults: 2,
+      threshold: 0.1,
+      asynch: true);
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadmodel() async{
+    await Tflite.loadModel(
+        model:"assest/model.tflite", labels:"assest/labels.txt");
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Emotion detector'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              height: MediaQuery.of(context).size.height*0.7,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController!.value.isInitialized?
+              Container():
+                  AspectRatio(aspectRatio: cameraController!.value.aspectRatio,
+                  child:CameraPreview(cameraController!),
+                  ),
+              ),
+          ),
+          Text(output,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          ),
+        ],
       ),
     );
   }
